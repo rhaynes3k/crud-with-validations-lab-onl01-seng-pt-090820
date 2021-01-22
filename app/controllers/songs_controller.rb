60f2ae03750908b8560b421bac0c12ddebc09918
rhaynes3k <rhaynes3k@gmail.com>
2021-01-21 22:06:34 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,24 @@
 class SongsController < ApplicationController
 
+  def new
+    @song = Song.new
+  end
+
   def index
     @song = Song.all
+    #binding.pry
   end
 
   def create
-    @song = Song.new(params)
+    #binding.pry
+    @song = Song.new(song_params)
     binding.pry
+  end
+
+private
+
+  def song_params
+    params.require(@song).permit(:title, :artist_name, :release_year, :released, :genre)
   end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,9 @@
 class Song < ApplicationRecord
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name]}
+
+  validates :artist_name, presence:true
+  validates :release_year,  presence:true, if: :released?
+
+
 end
